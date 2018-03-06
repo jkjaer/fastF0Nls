5de158ea1b94b697dfd6ad84b4b031bf8f2422c7
Jesper Kjær Nielsen <jkn@create.aau.dk>
2018-03-06 15:17:44 +0100

    Updated the documentation of the object

--- matlab/fastF0Nls.m
+++ matlab/fastF0Nls.m
@@ -1,100 +1,130 @@
 classdef fastF0Nls < handle
 %
-% Computes the non linear least-squares cost function of the real
-% harmonic model with additive noise
+% Computes an estimate of the fundamental frequency from a real-valued
+% signal segment.
 %
-%  data(n) = a_0 + sum_i a_i cos(i 2 \pi f_0 n) - b_i sin(i 2 \pi f_0 n) + e(n)
+% The signal segment is modelled as
+%  data(n) = a_0 + sum_i [ a_i cos(i 2 \pi f_0 n) - b_i sin(i 2 \pi f_0 n)] + e(n)
 % 
-% where L is the maxModelOrder. I.e. the objective is
-%
-%  f_l(w) = sum_n (a_0 sum_i^l a_i cos(i 2 \pi f n) 
-%                - b_i sin(i 2 \pi f n) - data(n) )^2
-%
-% From the above objective the cost J_l(w) = sum_n data(n)^2 -
-% f_l(w) is returned in CostFunctionMatrix for l=1:MaxModelOrder.
+% where the sum is over L harmonic components. The scalar a_0 is the 
+% DC-value. By default this is not included in the model.
 %     
 % Constructor input:
 %
 %    N: Signal length (integer, scalar)    
 %    
-%    L: maxModelOrder that is considered (integer, scalar).
+%    L: maximum model number of harmonics (i.e., order) that is expected
+%    (positive integer, scalar)
 %
-%    pitchBounds: Additional bounds of the cost function if tighter
-%           bounds are known for the fundamental frequency w_0. A two
-%           vector e.g. [0.001 0.49]. 0.0 is DC, 0.5 is the Nyquist.
+%    pitchBounds: Lower and upper bounds on the fundamental frequency in
+%           cycles/sample. The lower bound should not be set lower than
+%           1/N and the upper bound can at most be 0.5 (i.e., the nyquist 
+%           frequency). The bounds are specified as a 1x2 vector, e.g., 
+%           [80/samplingFreq 400/samplingFreq].
 %
-%    DC: Optional (default false). 
-%          If true, includes dc, a_0 in the model
+%    DC: Optional (default false).
+%          If true, includes a_0 (i.e., a DC-term) in the model
 %
 %    F: Optional (default 5*N*L)
-%           FFT length. Grid resolution is approximately 2/F (integer, scalar)
+%           FFT length (integer, scalar). Grid resolution is approximately
+%           samplingFreq/F Hz. Do not change this unless you know what 
+%           you are doing. If you need a finer resolution of the pitch
+%           estimate, please set the tolerance of the 'estimate' method 
+%           (see below).
 %    
 %    epsilon: regularization (default 0.0)
 %           Adds epsilon*N to the diagonal of the linear
-%           system. Useful for large L.
+%           system. Set this to, e.g., 1e-4 if your lowest candidate 
+%           fundamental frequency is smaller than 1 cycle/segment.
 %    
 % returns a fastF0Nls handle.
 %
 % Methods:
 %
-%   fastF0Nls.computeCostFunctions(x): Computes all the cost functions
-%
-%     Input: 
-%    
-%         x: signal (vector, length N)    
-%
-%     Output:
-%
-%         costFunctionMatrix: a matrix with the objective, size
-%           (nPitches x L). NaN indicates that the objective
-%           has not been evaluated due to the given pitch_bounds or because
-%           it is only necessary to evalute 
-%           f \in [pitchBounds(1), min(pitchBounds(2), 0.5/l)] at
-%           model order l = 1,...,maxModelOrder.
-%
-%         fullPitchGrid: (optional) values for the frequency grid 
-%                (real, size nPitches x 1)
-%   
-%
 %   fastF0Nls.estimate(x, tolerance): 
 %          Computes estimates of fundamental frequency and model order.
 %       
-%      Input:
+%       Input:
 %    
-%         x:    signal (vector, length N)    
-%         tolerance: (Optional, default is 1/F). Refinement 
-%           tolerance in unit periods/sample.
+%           x: signal (vector, length N)    
+%           tolerance: (Optional, default is 1/F). Refinement 
+%               tolerance in cycles/sample.
+%
+%       Output:
+%          
+%           f0: (optional) estimated fundamental frequency in
+%               cycles/sample.
+%        
+%           l: (optional) estimated number of harmonics/model order
+% 
+%           alpha: (optional) estimated linear parameters organised as
+%               [a_0 a_1 ... a_l b_1 ... b_l]
+%    
+%
+%   fastF0Nls.computeCostFunctions(x): Computes all the cost functions for
+%   every candidate model order and fundamental frequency.
+%
+%     Input: 
+%    
+%           x: signal (vector, length N)    
 %
 %     Output:
-%          
-%        f0:    (optional) estimated fundamental frequency. Units periods/sample
-%        
-%        l :    (optional) estimated model order
-% 
-%        alpha: (optional) linear parameters
-%    
-% This object implements a fast approach of evaluating the
-%  objective function on a uniform grid for all model order.
 %
+%           costFunctionMatrix: a matrix with the objective, size
+%               (nPitches x L). NaN indicates that the objective
+%               has not been evaluated due to the given pitch_bounds or 
+%               because it is only necessary to evalute 
+%               f \in [pitchBounds(1), min(pitchBounds(2), 0.5/l)] for the
+%               model orders l = 1,...,L.
+%
+%           fullPitchGrid: (optional) values for the frequency grid 
+%               (real, size nPitches x 1)
 %
 % Based on the papers:
 %
-%      Fast Fundamental Frequency Estimation: Making a
-%      Statistically Efficient Estimator Computationally Efficient
-%      J. K. Nielsen, T. L. Jensen, R. R. Jensen,
-%      M. G. Christensen, S. H. Jensen.
-%      Submitted, 2016
+%      Fast fundamental frequency estimation: Making a statistically 
+%      efficient estimator computationally efficient. Nielsen, Jesper Kjær;
+%      Jensen, Tobias Lindstrøm; Jensen, Jesper Rindom; Christensen, 
+%      Mads Græsbøll; Jensen, Søren Holdt. In: Signal Processing, 135, 
+%      2017, pp. 188-197.
 %
 %      and
 %
-%      Fast and Statistically Efficient Fundamental Frequency Estimation
-%      J. K. Nielsen, T. L. Jensen, R. R. Jensen,
-%      M. G. Christensen, S. H. Jensen.
-%      ICASSP, 2016
+%      Bayesian Model Comparison With the g-Prior. Nielsen, Jesper Kjær; 
+%      Christensen, Mads Græsbøll; Cemgil, Ali Taylan; Jensen, Søren 
+%      Holdt. In: IEEE Transactions on Signal Processing, 62 (1), 2014, 
+%      pp. 225-238.
+%
+% Example (analyse speech):
+%
+% % load the mono speech signal
+% [speechSignal, samplingFreq] = audioread('roy.wav');
+% nData = length(speechSignal);
+%
+% % set up
+% segmentTime = 0.025; % seconds
+% segmentLength = round(segmentTime*samplingFreq); % samples
+% nSegments = floor(nData/segmentLength);
+% f0Bounds = [80, 400]/samplingFreq; % cycles/sample
+% f0Res = 0.1/samplingFreq; % cycles/sample
+% maxNoHarmonics = 15;
+% f0Estimator = fastF0Nls(segmentLength, maxNoHarmonics, f0Bounds);
+%
+% % do the analysis
+% idx = 1:segmentLength;
+% f0Estimates = nan(1,nSegments); % cycles/sample
+% for ii = 1:nSegments
+%     speechSegment = speechSignal(idx);
+%     f0Estimates(ii) = f0Estimator.estimate(speechSegment, f0Res);
+%     idx = idx + segmentLength;
+% end
 %
 % Authors:  
-%      J. K. Nielsen, kjn@es.aau.dk
-%      T. L. Jensen, tlj@es.aau.dk
+%      Jesper Kjær Nielsen
+%      Tobias Lindstrøm Jensen
+% 
+% This file was obtained from https://github.com/jkjaer/fastF0Nls. Please
+% visit this page for more information on, e.g., licensing.
 %
     
     % Can only be set in the constructor
@@ -187,7 +217,7 @@ classdef fastF0Nls < handle
                 warning(['The lower pitch bound is set lower than one '...
                     ' period/segment. Inaccurate results might be '...
                     'produced - especially if you do not set the'...
-                    'regularisation parameter.']);
+                    ' regularisation parameter.']);
             end
 
             % Init
